cddbc1431a258f92c4d8e93d613e3154a239f421
fahimferoje <fabrahamjohn@gmail.com>
2019-08-30 12:34:05 +0600

    Sec 4 ep 22: Single user component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
 
 const UserItem = ({user: {login, avatar_url, html_url}}) => {
@@ -14,10 +15,10 @@ const UserItem = ({user: {login, avatar_url, html_url}}) => {
 			<h3> {login} </h3>
 
 			<div>
-				<a href={html_url} className="btn btn-dark btn-sm my-l">
+				<Link to={`/user/${login}`} className="btn btn-dark btn-sm my-l">
 					{" "}
 					More{" "}
-				</a>
+				</Link>
 			</div>
 		</div>
 	);
